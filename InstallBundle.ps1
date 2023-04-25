bb3b3003ec8f3293880bd000c865f007648f625d
jangid0604 <maheshjangidofficial@gmail.com>
2023-04-25 08:31:19 +0530

    Modified InstallBundle file

--- InstallBundle.ps1
+++ InstallBundle.ps1
@@ -1,1 +1,14 @@
-﻿
+﻿$PATH1 = 'E:\Testing.zip'
+$PATH2 = 'E:\unzip'
+$PATH3 = 'E:\Automation Script'
+
+#Converting zip folder to unzip folder
+Expand-Archive -LiteralPath $PATH1 -DestinationPath $PATH2
+
+
+#Copying unzip folder to another location
+Copy-Item -Path $PATH2 -Destination $PATH3 -Recurse
+
+
+#Removing earlier zip folder
+Remove-Item $PATH1
